92d2505021d1e59c94640a2e84077c542232c89f
ivanlytovka <ivanlytovka@gmail.com>
2025-05-13 19:52:27 -0400

    add callback tasks and solutions from lectures

---
+++ week2/callbacks/1-sync-sol.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// Task: rewrite function to return result into sync callback
+
+// Change signature to: (items, callback(result))
+const total = (items, callback) => {
+  let result = 0;
+  for (const item of items) {
+    result += item.price;
+  }
+  return callback(result);
+};
+
+const electronics = [
+  { name: 'Laptop', price: 1500 },
+  { name: 'Keyboard', price: 100 },
+  { name: 'HDMI cable', price: 10 },
+];
+
+// Use new signature total(electronics, (money) => ...)
+const money = total(electronics, (money) => {
+  console.log({ money });
+});
---
+++ week2/callbacks/readme.md
@@ -0,0 +1,1 @@
+This folder contains tasks and solutions from the [Callbacks repo](https://github.com/HowProgrammingWorks/Callbacks/tree/master/JavaScript/Tasks)
