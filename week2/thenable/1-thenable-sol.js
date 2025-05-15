ec9283f41318d9f7b5a6123b6d7b03c2d59ffb43
ivanlytovka <ivanlytovka@gmail.com>
2025-05-15 09:17:14 -0400

    solve 1-thenable

---
+++ week2/thenable/1-thenable-sol.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// Task: change `iterate` contract from callbacks to Thenable
+
+const iterate = (item) => ({
+  then: function(onFulfilled) {
+    onFulfilled(item);
+  }
+});
+
+const electronics = [
+  { name: 'Laptop', price: 1500 },
+  { name: 'Keyboard', price: 100 },
+  { name: 'HDMI cable', price: 10 },
+];
+
+// Use await syntax to get items
+(async () => {
+  for (const item of electronics) {
+    const awaitedItem = await iterate(item)
+    console.log(awaitedItem)
+  }
+})();
