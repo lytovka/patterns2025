6d09eea93b00585d0cf774070455fb4d092e128e
ivanlytovka <ivanlytovka@gmail.com>
2025-05-17 13:09:57 -0400

    reduce iterations over table

--- week1/utils/renderer.js
+++ week1/utils/renderer.js
@@ -1,5 +1,4 @@
 import {
-  isObject,
   isNonEmptyArray,
   intersectingKeys,
   maxLengthElement,
@@ -15,28 +14,28 @@ function getPaddedRow(row, gaps) {
 }
 
 export function formatToTable(table, options = { gap: DEFAULT_COLUMN_GAP }) {
-  const isListOfObjects =
-    isNonEmptyArray(table) && table.every((entry) => isObject(entry));
-  if (!isListOfObjects) return;
-
   const { gap } = options;
 
   // find unique columns
   const columns = intersectingKeys(table);
 
+  if (!columns || columns.length === 0) return
+
   // find minimum offset width for each column
   const columnMinWidths = Array.from({ length: columns.length });
   for (let i = 0; i < columns.length; i++) {
     const currentColumn = columns[i];
-    columnMinWidths[i] = maxLengthElement([
-      ...table.map((entry) => entry[currentColumn]),
-      currentColumn,
-    ]);
+    const columnValues = table.map((entry) => String(entry[currentColumn] || ''))
+    columnValues.push(currentColumn)
+    columnMinWidths[i] = maxLengthElement(columnValues)
   }
+
   const paddings = columnMinWidths.map((v) => v + gap);
 
-  // render
+  // render header
   let renderedTable = getPaddedRow(columns, paddings);
+
+  // render rows
   for (let i = 0; i < table.length; i++) {
     const row = Object.values(table[i]);
     const renderedRow = getPaddedRow(row, paddings);
