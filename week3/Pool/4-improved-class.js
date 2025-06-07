ce8ea86dee6a7b8731309afc9c78b71ab02236e4
ivanlytovka <ivanlytovka@gmail.com>
2025-06-07 00:03:29 -0400

    add class impl

---
+++ week3/Pool/4-improved-class.js
@@ -0,0 +1,37 @@
+'use strict';
+
+class Poolify {
+  constructor(factory, options, size, max) {
+    this.instances = new Array(max).fill(factory(...options), 0, size);
+  }
+
+  acquire() {
+    const index = this.instances.findIndex(Boolean)
+    if (index === -1) throw new Error("Cannot acquire instance in emtpy pool")
+    const freeInstance = this.instances[index]
+    this.instances[index] = null
+    return freeInstance
+  }
+
+  push(instance) {
+    const index = this.instances.findIndex(instance => !Boolean(instance))
+    if (index === -1) throw new Error("Cannot push instance to the full pool")
+    this.instances[index] = instance
+  }
+}
+
+// Usage
+const maxSize = 15
+const minSize = 10
+
+const createBuffer = (size) => new Uint8Array(size);
+const pool = new Poolify(createBuffer, [4096], minSize, maxSize);
+
+for (let i = 0; i < minSize; i++) {
+  const instance = pool.acquire();
+  console.log({ instance });
+}
+
+for (let i = 0; i < maxSize; i++) {
+  pool.push(createBuffer);
+}
