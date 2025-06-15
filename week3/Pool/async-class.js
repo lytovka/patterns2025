edb584ab8dac4bf692afe73052dd916c50fb4e6d
ivanlytovka <ivanlytovka@gmail.com>
2025-06-15 13:24:36 -0400

    make props private and consolidate instances and flags

--- week3/Pool/async-class.js
+++ week3/Pool/async-class.js
@@ -10,85 +10,79 @@ class PoolError extends Error {
 const DEFAULT_POOL_MAX_SIZE = 15;
 
 class Poolify {
-  instances = [];
-  freeInstances = [];
-  queue = [];
-  maxSize = 0;
-  currentIndex = 0;
-  availableInstances = 0;
+  #instances = [];
+  #queue = [];
+  #maxSize = 0;
+  #currentIndex = 0;
+  #availableInstances = 0;
 
   constructor(options = {}) {
-    this.maxSize = options.max || DEFAULT_POOL_MAX_SIZE;
-    this.instances = new Array(this.maxSize);
-    this.freeInstances = new Array(this.maxSize).fill(false);
+    this.#maxSize = options.max || DEFAULT_POOL_MAX_SIZE;
+    this.#instances = new Array(this.#maxSize).fill({ instance: null, isFree: false });
   }
 
   add(instance) {
-    if (this.availableInstances === this.maxSize) {
+    if (this.#availableInstances === this.#maxSize) {
       throw new PoolError("attempt to add instance to the full pool");
     }
-    if (this.instances.includes(instance)) {
+    if (this.#instances.findIndex(i => i.instance === instance) !== -1) {
       throw new PoolError("attempt to add duplicate instance");
     }
-    this.instances[this.currentIndex] = instance;
-    this.freeInstances[this.currentIndex] = true;
-    this.availableInstances++;
-    this.currentIndex++;
+    this.#instances[this.#currentIndex] = { instance, isFree: true };
+    this.#availableInstances++;
+    this.#currentIndex++;
   }
 
   addAll(...instances) {
-    if (this.availableInstances === this.maxSize) {
-      throw new PoolError("attempt to add instance to the full pool");
-    }
-    if (instances.length > this.maxSize) {
-      throw new PoolError("attempt to exceed the size of the pool");
-    }
-    instances.forEach((instance) => {
-      this.add(instance);
-    });
+    for (const instance of instances) this.add(instance)
   }
 
-  #acquire(callback) {
-    if (this.availableInstances === 0) {
-      this.queue.push(callback);
-      return;
+  #findFreeInstance(callback) {
+    if (this.#availableInstances === 0) {
+      this.#queue.push(callback);
+      return { currInstance: null };
     }
     let currInstance = null;
-    let currIndex = this.currentIndex
+    let currIndex = this.#currentIndex
     let isFree = false;
     while (!currInstance || !isFree) {
-      currIndex = this.currentIndex
-      currInstance = this.instances[currIndex];
-      isFree = this.freeInstances[this.currentIndex];
-      this.currentIndex++;
-      if (this.currentIndex > this.maxSize - 1) this.currentIndex = 0;
+      // console.log({ currInstance, isFree, currIndex })
+      currIndex = this.#currentIndex
+      currInstance = this.#instances[this.#currentIndex].instance;
+      isFree = this.#instances[this.#currentIndex].isFree;
+      this.#currentIndex++;
+      if (this.#currentIndex > this.#maxSize - 1) this.#currentIndex = 0;
     }
     return { currInstance, currIndex };
   }
 
   capture(callback) {
-    const { currInstance, currIndex } = this.#acquire(callback);
+    const { currInstance, currIndex } = this.#findFreeInstance(callback);
     if (!currInstance)
       return callback(new PoolError("no free instances found"), null);
-    this.freeInstances[currIndex] = false;
-    this.availableInstances--;
+    this.#instances[currIndex].isFree = false;
+    this.#availableInstances--;
     return callback(null, currInstance);
   }
 
   release(instance, callback) {
-    const index = this.instances.indexOf(instance);
-    if (index === -1)
-      return callback(new PoolError("cannot release foreign instance", null));
-    if (this.freeInstances[index])
-      return callback(new PoolError("instance was not previously acquired."));
-    this.availableInstances++;
-    this.freeInstances[index] = true;
-    if (this.queue.length > 0) {
-      const nextCallback = this.queue.shift();
+    const index = this.#instances.findIndex(i => i.instance === instance);
+    if (index === undefined) {
+      callback(new PoolError("cannot release foreign instance", null));
+      return
+    }
+    if (this.#instances[index].isFree) {
+      callback(new PoolError("instance was not previously acquired."));
+      return
+    }
+    this.#availableInstances++;
+    this.#instances[index].isFree = true;
+    if (this.#queue.length > 0) {
+      const nextCallback = this.#queue.shift();
       if (nextCallback) {
         setImmediate(() => {
-          this.freeInstances[index] = false;
-          this.availableInstances--;
+          this.#instances[index].isFree = false;
+          this.#availableInstances--;
           nextCallback(null, instance);
         });
       }
@@ -108,11 +102,11 @@ const main = () => {
   pool.add(instance1);
   pool.addAll(instance2, instance3);
   // pool.add(instance1)
-
-  /* for (let i = 4; i < max+1; i++) {
-    const instance = { instance: i }
-    pool.add(instance)
-  } */
+  //
+  // for (let i = 4; i < max + 2; i++) {
+  //   const instance = { instance: i }
+  //   pool.add(instance)
+  // }
 
   const callback = (type) => (err, instance) => {
     if (err) return console.error(err);
@@ -122,6 +116,7 @@ const main = () => {
   pool.capture(callback("capture"));
   pool.capture(callback("capture"));
   pool.capture(callback("capture"));
+  // pool.capture(callback("capture"));
   // pool.capture(callback("capture"));
   pool.release(instance2, callback("release"));
   pool.release(instance1, callback("release"));
