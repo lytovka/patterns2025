3a0092111693dfa74de152a8ae5414a0776d1762
ivanlytovka <ivanlytovka@gmail.com>
2025-06-24 19:52:44 -0400

    pt2: add isResolved

--- week5/Adapter/2-abort-solution.js
+++ week5/Adapter/2-abort-solution.js
@@ -7,24 +7,28 @@ import fs from 'node:fs';
 
 const promisify = (fn, options) => (...args) => {
   const { timeout = 1000, signal: externalSignal } = options
+  let isResolved = false
   const timer = AbortSignal.timeout(timeout)
-  const signal = AbortSignal.any([timer, externalSignal])
+  const combinedSignal = AbortSignal.any([timer, externalSignal])
   const promise = new Promise((resolve, reject) => {
-    signal.addEventListener('abort', () => {
+    combinedSignal.addEventListener('abort', () => {
+      if (isResolved) return
       reject(new Error("timeout exceeded"));
     });
 
-    const callback = (err, data) => {
+    fn(...args, (err, data) => {
+      if (isResolved) return
       if (err) reject(err);
       else resolve(data);
-    };
-    fn(...args, callback);
+    });
   });
-  return promise;
+  return promise.finally(() => {
+    isResolved = true
+  });
 };
 
 // Usage
-const read = promisify(fs.readFile, { timeout: 100, signal: AbortSignal.timeout(1) });
+const read = promisify(fs.readFile, { timeout: 100, signal: AbortSignal.timeout(10) });
 
 const main = async () => {
   const fileName = '2-abort-solution.js';
