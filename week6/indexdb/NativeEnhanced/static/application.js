e055c63d1d785f82ebda943f5d5a40aba3792efa
Ivan Lytovka <ivanlytovka@gmail.com>
2025-07-11 15:10:25 -0400

    format

--- week6/indexdb/NativeEnhanced/static/application.js
+++ week6/indexdb/NativeEnhanced/static/application.js
@@ -27,31 +27,31 @@ function withResolvers() {
 }
 
 const OPERATIONS = {
-  "greater": (a, b) => a > b,
-  "smaller": (a, b) => a < b
-}
+  greater: (a, b) => a > b,
+  smaller: (a, b) => a < b,
+};
 
 const TRANSACTION_MODES = {
-  READONLY: "readonly",
-  READWRITE: "readwrite",
-  VERSIONCHANGE: "versionchange"
-}
+  READONLY: 'readonly',
+  READWRITE: 'readwrite',
+  VERSIONCHANGE: 'versionchange',
+};
 
 class IndexedDb extends EventTarget {
-  #db
+  #db;
 
   constructor(db) {
-    super()
-    this.#db = db
+    super();
+    this.#db = db;
   }
 
   static async build(name, version) {
-    const db = await this.#initDB(name, version)
-    return new IndexedDb(db)
+    const db = await this.#initDB(name, version);
+    return new IndexedDb(db);
   }
 
   static async #initDB(name, version) {
-    const { promise, resolve, reject } = withResolvers()
+    const { promise, resolve, reject } = withResolvers();
     const request = window.indexedDB.open(name, version);
 
     // TODO: needs to be model-agnostic
@@ -64,65 +64,73 @@ class IndexedDb extends EventTarget {
     request.onsuccess = () => resolve(request.result);
     request.onerror = () => reject(request.error);
 
-    return promise
+    return promise;
   }
 
   async insert(storeName, record) {
-    const { promise, resolve, reject } = withResolvers()
+    const { promise, resolve, reject } = withResolvers();
     const tx = this.#db.transaction(storeName, TRANSACTION_MODES.READWRITE);
 
     tx.objectStore(storeName).add(record);
     tx.oncomplete = () => {
-      this.#logAndFulfill("insert", record, resolve)
-    }
+      this.#logAndFulfill('insert', record, resolve);
+    };
     tx.onerror = () => {
-      this.#logAndFulfill("insert", new Error("Could not add record"), reject)
-    }
+      this.#logAndFulfill('insert', new Error('Could not add record'), reject);
+    };
 
-    return promise
+    return promise;
   }
 
   async readAll(storeName) {
-    const { promise, resolve, reject } = withResolvers()
+    const { promise, resolve, reject } = withResolvers();
     const tx = this.#db.transaction(storeName, TRANSACTION_MODES.READONLY);
     const store = tx.objectStore(storeName);
     const req = store.getAll();
 
     req.onsuccess = () => {
-      this.#logAndFulfill("readAll", req.result, resolve)
-    }
+      this.#logAndFulfill('readAll', req.result, resolve);
+    };
     req.onerror = () => {
-      this.#logAndFulfill("readAll", new Error("Could not read record"), reject)
-    }
+      this.#logAndFulfill(
+        'readAll',
+        new Error('Could not read record'),
+        reject,
+      );
+    };
 
-    return promise
+    return promise;
   }
 
   // example: {age: {greater: 18}}
   async readSingle(storeName, id) {
-    const { promise, resolve, reject } = withResolvers()
+    const { promise, resolve, reject } = withResolvers();
     const tx = this.#db.transaction(storeName, TRANSACTION_MODES.READONLY);
     const store = tx.objectStore(storeName);
-    const req = store.get(id)
+    const req = store.get(id);
 
     req.onsuccess = () => {
       const user = req.result;
       if (!user) {
-        this.#logAndFulfill("readSingle", new Error(`User with id=${id} not found`), reject);
+        this.#logAndFulfill(
+          'readSingle',
+          new Error(`User with id=${id} not found`),
+          reject,
+        );
         return;
       }
-      this.#logAndFulfill("readSingle", user, resolve)
+      this.#logAndFulfill('readSingle', user, resolve);
     };
     req.onerror = () => {
-      this.#logAndFulfill("readSingle", new Error('Update failed'), reject);
-    }
+      this.#logAndFulfill('readSingle', new Error('Update failed'), reject);
+    };
 
-    return promise
+    return promise;
   }
 
   // example: {age: {greater: 18}}
   async read(storeName, condition = {}) {
-    const { promise, resolve, reject } = withResolvers()
+    const { promise, resolve, reject } = withResolvers();
     const tx = this.#db.transaction(storeName, TRANSACTION_MODES.READONLY);
     const store = tx.objectStore(storeName);
     const req = store.openCursor();
@@ -131,24 +139,34 @@ class IndexedDb extends EventTarget {
     req.onsuccess = (event) => {
       const cursor = event.target.result;
       if (!cursor) {
-        this.#logAndFulfill("read", result, resolve)
-        return
+        this.#logAndFulfill('read', result, resolve);
+        return;
       }
       const user = cursor.value;
-      const [objKey, operation] = Object.entries(condition)[0]
-      const [operationKey, value] = Object.entries(operation)[0]
-      if (OPERATIONS[operationKey](user[objKey], value) || !condition || !operation) result.push(user);
+      const [objKey, operation] = Object.entries(condition)[0];
+      const [operationKey, value] = Object.entries(operation)[0];
+      const op = OPERATIONS[operationKey] || null
+      if (!op) {
+        result.push(user)
+      }
+      else if (
+        op(user[objKey], value) ||
+        !condition ||
+        !operation
+      )
+        result.push(user);
+
       cursor.continue();
     };
     req.onerror = () => {
-      this.#logAndFulfill("read", new Error('Adult query failed'), reject)
-    }
+      this.#logAndFulfill('read', new Error('Adult query failed'), reject);
+    };
 
-    return promise
+    return promise;
   }
 
   async update(storeName, id) {
-    const { promise, resolve, reject } = withResolvers()
+    const { promise, resolve, reject } = withResolvers();
     const tx = this.#db.transaction(storeName, TRANSACTION_MODES.READWRITE);
     const store = tx.objectStore(storeName);
     const req = store.get(id);
@@ -156,79 +174,82 @@ class IndexedDb extends EventTarget {
     req.onsuccess = () => {
       const user = req.result;
       if (!user) {
-        this.#logAndFulfill("update", new Error(`User with id=${id} not found`), reject);
-        return
+        this.#logAndFulfill(
+          'update',
+          new Error(`User with id=${id} not found`),
+          reject,
+        );
+        return;
       }
       user.age += 1;
       store.put(user);
       tx.oncomplete = () => {
-        this.#logAndFulfill("update", user, resolve);
-      }
+        this.#logAndFulfill('update', user, resolve);
+      };
     };
     req.onerror = () => {
-      this.#logAndFulfill("update", new Error('Update failed'), reject);
-    }
-    return promise
+      this.#logAndFulfill('update', new Error('Update failed'), reject);
+    };
+    return promise;
   }
 
   async delete(storeName, id) {
-    const { promise, resolve, reject } = withResolvers()
-    return this.readSingle(storeName, id).then((record) => {
-      const tx = this.#db.transaction(storeName, TRANSACTION_MODES.READWRITE)
+    const { promise, resolve, reject } = withResolvers();
+    return await this.readSingle(storeName, id).then((record) => {
+      const tx = this.#db.transaction(storeName, TRANSACTION_MODES.READWRITE);
       tx.objectStore(storeName).delete(record.id);
 
       tx.oncomplete = () => {
-        this.#logAndFulfill("delete", record, resolve);
-      }
+        this.#logAndFulfill('delete', record, resolve);
+      };
       tx.onerror = () => {
-        this.#logAndFulfill("delete", new Error('Delete failed'), reject);
-      }
+        this.#logAndFulfill('delete', new Error('Delete failed'), reject);
+      };
 
-      return promise
-    })
+      return promise;
+    }, reject)
   }
 
+  // TODO: encapsulate emitting and resolve in different methods
   #logAndFulfill(type, value, resolver) {
-    const v = value instanceof Error ? value.message : value
-    this.dispatchEvent(
-      new CustomEvent("log", { detail: { type, value: v } })
-    )
-    resolver(value)
+    const v = value instanceof Error ? value.message : value;
+    this.dispatchEvent(new CustomEvent('log', { detail: { type, value: v } }));
+    resolver(value);
   }
 }
 
 // Usage
-const logger = new Logger("output")
-const iDB = await IndexedDb.build("Example", 1)
+const logger = new Logger('output');
+const iDB = await IndexedDb.build('Example', 1);
 
-iDB.addEventListener("log", (event) => {
-  logger.log(event.detail)
-})
+iDB.addEventListener('log', (event) => {
+  logger.log(event.detail);
+});
 
 document.getElementById('add').onclick = async () => {
   const name = prompt('Enter user name:');
   if (!name) return;
   const age = parseInt(prompt('Enter age:'), 10);
   if (!Number.isInteger(age)) return;
-  await iDB.insert("user", { name, age })
+  await iDB.insert('user', { name, age });
 };
 
 document.getElementById('get').onclick = async () => {
-  await iDB.readAll("user")
+  await iDB.readAll('user');
 };
 
 document.getElementById('update').onclick = async () => {
-  const id = parseInt(prompt("Enter ID"), 10)
-  if (!Number.isInteger(id)) return
-  await iDB.update("user", id)
+  const id = parseInt(prompt('Enter ID'), 10);
+  if (!Number.isInteger(id)) return;
+  await iDB.update('user', id);
 };
 
 document.getElementById('delete').onclick = async () => {
-  const id = parseInt(prompt("Enter ID"), 10)
-  if (!Number.isInteger(id)) return
-  await iDB.delete("user", id)
+  const id = parseInt(prompt('Enter ID'), 10);
+  if (!Number.isInteger(id)) return;
+  await iDB.delete('user', id);
 };
 
 document.getElementById('adults').onclick = async () => {
-  await iDB.read("user", { age: { greater: 18 } })
+  await iDB.read('user', { age: { greater: 18 } });
 };
