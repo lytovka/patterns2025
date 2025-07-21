c94dd5b0832c13dc7d62e2bdd5f3a054ea571851
Ivan Lytovka <ivanlytovka@gmail.com>
2025-07-20 21:32:02 -0400

    add OPFS wrapper with error handling

--- week8/file-api/App/static/application.js
+++ week8/file-api/App/static/application.js
@@ -1,20 +1,4 @@
-class Logger {
-  #output;
-
-  constructor(outputId) {
-    this.#output = document.getElementById(outputId);
-  }
-
-  log(...args) {
-    const lines = args.map(Logger.#serialize);
-    this.#output.textContent += lines.join(" ") + "\n";
-    this.#output.scrollTop = this.#output.scrollHeight;
-  }
-
-  static #serialize(x) {
-    return typeof x === "object" ? JSON.stringify(x, null, 2) : x;
-  }
-}
+import Logger from "./logger.js";
 
 const logger = new Logger("output");
 
